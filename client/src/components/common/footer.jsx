024d929e5cd805303d8580d794d0cbfded7f9868
Nikola <nkrezeski@banzae.dev>
2022-08-31 01:56:20 +0200

    (ref) change footer so that the year is dynamically updated

--- client/src/components/common/footer.jsx
+++ client/src/components/common/footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="container-md text-color-white d-flex justify-content-end">
-      © Copyright 2022 All Rights Reserved
+      © Copyright {new Date().getFullYear()} All Rights Reserved
     </footer>
   )
 }
